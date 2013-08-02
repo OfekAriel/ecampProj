4f863df0edc76314e6691d3ae4a4dfc5470feb80
unknown <staff@eCamp-Sh3-16.root.eCamp>
2013-08-02 09:45:35 +0300

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoolShit::Application.config.secret_key_base = '835096667d4d2ecc912fd11724e83f1825b419b87cf87cbd98e2d8cb5e2584a4249e4598c09e6b8bda761243ee48c725e842e3076dc214e6fa373b2c93646d3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoolShit::Application.config.session_store :cookie_store, key: '_coolShit_session'
